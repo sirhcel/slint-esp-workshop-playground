6ef001eef62744647d676c2d74312f369b47f443
Christian Meusel <christian.meusel@posteo.de>
2024-05-28 16:16:34 +0200

    Show first actual sensor data

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,43 @@
+use std::sync::mpsc;
+use std::time::{Duration, Instant};
 
 mod dht22;
 mod esp32;
 
 slint::include_modules!();
 
-unsafe extern "C" fn dht_task(_: *mut core::ffi::c_void) {
+struct SensorData {
+    temperature_celsius: f32,
+    humidity_percent: f32,
+    when: Duration,
+}
+
+fn dht_task(tx: mpsc::Sender<SensorData>) {
+    // let delay = Delay::new_default();
+    let start = Instant::now();
     let dht = dht22::DHT22::new(13);
 
     loop {
         match dht.read() {
             #[allow(unused_variables)]
             Ok((temperature, humidity)) => {
-                // TODO: Store sensor values somewhere
-                
+                let data = SensorData {
+                    temperature_celsius: temperature,
+                    humidity_percent: humidity,
+                    when: Instant::now().duration_since(start),
+                };
+                tx.send(data).unwrap_or_else(|e| {
+                    log::error!("Sending sensor data failed: {:?}", e);
+                });
             }
             Err(e) => {
                 log::error!("Error reading DHT22: {:?}", e);
             }
         }
 
-        esp_idf_svc::sys::vTaskDelay(2000 / 10);
+        unsafe {
+            esp_idf_svc::sys::vTaskDelay(2000 / 10);
+        }
     }
 }
 
@@ -34,27 +52,35 @@ fn main() {
     // Set the platform
     slint::platform::set_platform(esp32::EspPlatform::new()).unwrap();
 
-    // Create DHT task
-    unsafe {
-        let mut task_handle = std::ptr::null_mut();
-        esp_idf_svc::sys::xTaskCreatePinnedToCore(
-            Some(dht_task),
-            b"sensor_task\0".as_ptr() as *const i8,
-            4096,
-            std::ptr::null_mut(),
-            5,
-            &mut task_handle,
-            1
-        );
-    }
+    let (sensor_tx, sensor_rx) = mpsc::channel::<SensorData>();
+    std::thread::spawn(move || dht_task(sensor_tx));
 
     // Finally, run the app!
     let ui = AppWindow::new().expect("Failed to load UI");
+    let ui_handle = ui.as_weak();
 
     let timer = slint::Timer::default();
-    timer.start(slint::TimerMode::Repeated, std::time::Duration::from_millis(2000), move || {
-        // TODO: Update UI with sensor values
-    });
+    timer.start(
+        slint::TimerMode::Repeated,
+        std::time::Duration::from_millis(2000),
+        move || {
+            let ui = ui_handle.unwrap();
+            let model = ViewModel::get(&ui);
+            match sensor_rx.recv_timeout(Duration::from_millis(10)) {
+                Ok(data) => {
+                    let when = data.when.as_secs().to_string();
+                    model.set_current(WeatherRecord {
+                        temperature_celsius: data.temperature_celsius,
+                        humidity_percent: data.humidity_percent,
+                        timestamp: slint::SharedString::from(when),
+                    });
+                }
+                Err(e) => {
+                    log::error!("Receiving sensor data failed: {:?}", e);
+                }
+            }
+        },
+    );
 
     ui.run().unwrap();
 }
